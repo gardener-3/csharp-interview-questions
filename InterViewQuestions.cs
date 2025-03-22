314c9c94ca0580c04b0dffb8fdce10fbdbcc9d85
gardener-3 <gardener-3@gmail.com>
2025-03-22 21:20:38 +0100

    Add the IsUnique method. Add unit tests

--- InterViewQuestions.cs
+++ InterViewQuestions.cs
@@ -7,7 +7,25 @@ public class InterViewQuestions
     // the question to ask is should we support ascii or unicode. if ascii then limited to 128 or 256 ?
     public bool IsUnique(string testString)
     {
+        var splitChars = testString.Replace(" ", "").ToCharArray();
+        var resultArray = new int[128];
+        foreach (var c in splitChars)
+        {
+            if (resultArray[(int)c] == 1){
+                return false;
+            }
+            else
+            {
+                resultArray[(int)c] = 1;
+            }
+            
+        }
+        return true;
+    }
+
+    public bool CheckPermutation(string testString)
+    {
+        
         return false;
     }
-    
 }
