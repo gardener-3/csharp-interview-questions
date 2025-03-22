c889eb1547e7c525fb9724b02f780dbc904c0934
gardener-3 <gardener-3@gmail.com>
2025-03-22 11:47:08 +0100

    initial commit

---
+++ InterViewQuestions.cs
@@ -0,0 +1,13 @@
+﻿namespace arrays_and_strings;
+
+public class InterViewQuestions
+{
+    //Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+    //cannot use additional data structures?
+    // the question to ask is should we support ascii or unicode. if ascii then limited to 128 or 256 ?
+    public bool IsUnique(string testString)
+    {
+        return false;
+    }
+    
+}
